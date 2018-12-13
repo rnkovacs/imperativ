90546505c2850ca73c67f899bc1fbf1e5e040c8e
Réka Kovács <rekanikolett@gmail.com>
2018-12-13 13:19:05 +0100

    Create gyakorlo1_megoldas.c

---
+++ gyakorlo1_megoldas.c
@@ -0,0 +1,53 @@
+/*
+TESZT:
+
+ 1: B
+ 2: D
+ 3: A
+ 4: B
+ 5: B
+ 6: B
+ 7: C
+ 8: D
+ 9: C
+10: B
+
+PROGRAMOZÁSI FELADAT:
+
+Írjon függvényt, amely egy adott sztringben megkeresi egy mintasztring utolsó előfordulásának kezdőindexét. Amennyiben a 
+minta nem található meg az adott sztringben, akkor a visszatérési érték legyen -1. Mutassa be a függvény működését.
+
+Példa input:
+"1111 0000 1011 1110 0000 1010 1011 1110", minta: "1011"
+
+Elvárt output:
+30
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+
+int strlastsample(const char str[], const char sample[])
+{
+    const int sample_len = strlen(sample);
+    int i, j = sample_len - 1;
+    for (i = strlen(str) - 1; i >= 0; --i)
+    {
+        if (str[i] == sample[j])
+        {
+            if (!j)
+                return i;
+            --j;
+        }
+        else
+            j = sample_len - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    printf("result: %d\n", strlastsample("1111 0000 1011 1110 0000 1010 1011 1110", "1011"));
+    return 0;
+}
